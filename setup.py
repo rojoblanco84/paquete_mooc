8ae6e817c53a3d3d9f2ee004b500f221e5f09622
Jorge Blanco <jblanco@ciencias.unam.mx>
2024-12-09 10:39:00 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mi_paquete',
+    version='0.1.0',
+    packages=find_packages(),
+    description='Descripción breve del paquete',
+    author='G',
+    author_email='tu_email@example.com',
+    url='URL del repositorio de GitHub',
+    install_requires=[
+        # Lista de dependencias necesarias
+    ],
+)
